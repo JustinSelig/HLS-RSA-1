15e29cc5d04cc216f3d983aa78c141db71bf0ea2
Justin Selig <jss459@cornell.edu>
2017-11-22 15:05:45 -0800

    fixed overflow issue in decrypt power function
    fixed overflow issue in decrypt power function

--- test_rsa.hpp
+++ test_rsa.hpp
@@ -6,8 +6,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <random>
+//#include <random>
 
-#include "timer.hpp"
+//#include "timer.hpp"
+#include "timer.h"
 
 #endif /* __TEST_HPP__ */
